a64e750f0860b88feee49df0d8341cf5773565f1
Adriana Calpa <adrianacalpab@gmail.com>
2021-03-02 21:56:55 -0500

    commit prueba conflicto

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -4,7 +4,7 @@
     <div class="row">       
 
         <div class="col-sm-4" align="center">           
-            {{-- TODO: Imagen de la película --}}  
+            {{-- TODO: Imagen de la películaaaaaaaaaaaaa --}}  
             <img src="{{$pelicula->poster}}" style="height:350px"/>    
         </div>      
 
